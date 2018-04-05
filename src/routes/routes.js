75091f8d55d3a13b6c6f6b7e51b8973c0c4ee293
Miguel Meza <boadude@gmail.com>
2018-04-04 23:31:55 -0300

    Detalle de acción se abre con Modal

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,32 +1,31 @@
 import React from "react";
-import {
-  Stack,
-  Scene,
-  CardStackStyleInterpolator
-} from "react-native-router-flux";
+import { Modal, Router, Scene } from "react-native-router-flux";
 
 import Dashboard from "@components/dashboard/Dashboard";
 import SalasInfo from "@components/salas_info/SalasInfo";
 import SalasInfoDetailAction from "@components/salas_info/salas_info_detal_action/SalasInfoDetailAction";
 
 const Index = (
-  <Stack hideNavBar>
-    <Scene key="dashboard" component={Dashboard} title="Dashboard" initial />
-    <Scene
-      key="salasInfo"
-      component={SalasInfo}
-      title="SalasInfo"
-      direction="horizontal"
-    />
-    <Scene
-      key="salasInfoDetailAction"
-      component={SalasInfoDetailAction}
-      title="SalasInfoDetailAction"
-      transitionConfig={() => ({
-        screenInterpolator: CardStackStyleInterpolator.forVertical
-      })}
-    />
-  </Stack>
+  <Router>
+    <Modal>
+      <Scene key="root" hideNavBar>
+        <Scene
+          key="dashboard"
+          component={Dashboard}
+          title="Dashboard"
+          initial
+        />
+        <Scene key="salasInfo" component={SalasInfo} title="SalasInfo" />
+      </Scene>
+
+      <Scene
+        hideNavBar
+        key="salasInfoDetailAction"
+        component={SalasInfoDetailAction}
+        title="SalasInfoDetailAction"
+      />
+    </Modal>
+  </Router>
 );
 
 export default Index;
