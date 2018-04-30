33c66b0e2d4f38a87ab40ec82d0b21ec5b2014c0
Ezequiel Obreque <eandresobrequev@gmail.com>
2018-04-30 01:15:47 -0300

    refactoring

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -52,15 +52,7 @@ class Maps extends Component {
           <Right />
         </Header>
 
-        <MapView
-          style={{ flex: 1 }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        />
+        <MapView style={{ flex: 1 }} initialRegion={region} />
       </Container>
     );
   };
