5a39991d4b279e049e059e41fd4300e31257a302
Ezequiel Obreque <eandresobrequev@gmail.com>
2018-05-18 00:09:31 -0400

    google maps

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -9,34 +9,50 @@ import {
   Button,
   Icon,
   Title,
-  Body,
+  Body
 } from "native-base";
 import { Actions } from "react-native-router-flux";
 import { MapView } from "expo";
 import GetRegionMaps from "@components/maps/MapsAction";
+import { ListadoSalas } from "@components/salas/salas_list/SalasListActions";
 
 class Maps extends Component {
   static propTypes = {
     GetRegionMaps: PropTypes.func.isRequired,
+    ListadoSalas: PropTypes.func.isRequired,
     region: PropTypes.shape({
       latitude: PropTypes.number,
       longitude: PropTypes.number,
       latitudeDelta: PropTypes.number,
       longitudeDelta: PropTypes.number
-    })
+    }),
+    salas: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        bandera: PropTypes.string,
+        date_b2b: PropTypes.string,
+        mide: PropTypes.number,
+        realizada: PropTypes.number,
+        fecha_visita: PropTypes.string,
+        direccion: PropTypes.string,
+        latitud: PropTypes.any,
+        longitud: PropTypes.an
+      })
+    )
   };
 
   static defaultProps = {
-    region: {}
+    region: {},
+    salas: []
   };
 
   componentWillMount = () => {
     this.props.GetRegionMaps();
+    this.props.ListadoSalas();
   };
 
   render = () => {
-    const { region } = this.props;
-    
+    const { salas, region } = this.props;
     return (
       <Container>
         <Header style={{ borderBottomWidth: 0 }}>
@@ -50,19 +66,69 @@ class Maps extends Component {
           </Body>
           <Right />
         </Header>
+        <MapView
+          style={{ flex: 1 }}
+          region={region}
+          // onRegionChange={this.onRegionChange}
+        >
+          {salas.map(sala => {
+            if (sala.latitud && sala.longitud) {
+              let logo = "";
+              if (sala.bandera === "JUMBO") {
+                logo = require("@assets/images/jumbo.png");
+              } else if (
+                sala.bandera === "LIDER EXPRESS" ||
+                sala.bandera === "LIDER"
+              ) {
+                logo = require("@assets/images/lider.png");
+              } else if (sala.bandera === "CENTRAL MAYORISTA") {
+                logo = require("@assets/images/central-mayorista.png");
+              } else if (sala.bandera === "TOTTUS") {
+                logo = require("@assets/images/tottus.png");
+              } else if (sala.bandera === "EKONO") {
+                logo = require("@assets/images/ekono.png");
+              } else if (sala.bandera === "ACUENTA") {
+                logo = require("@assets/images/acuenta.png");
+              } else if (sala.bandera === "SANTA ISABEL") {
+                logo = require("@assets/images/santaisabel.png");
+              } else if (sala.bandera === "UNIMARC") {
+                logo = require("@assets/images/unimarc.png");
+              } else if (sala.bandera === "MAYORISTA 10") {
+                logo = require("@assets/images/mayorista10.png");
+              } else if (sala.bandera === "ALVI") {
+                logo = require("@assets/images/alvi.png");
+              } else {
+                logo = require("@assets/images/alvi.png");
+              }
 
-        <MapView style={{ flex: 1 }} initialRegion={region} />
+              return (
+                <MapView.Marker.Animated
+                  key={sala.cod_local}
+                  coordinate={{
+                    latitude: parseFloat(sala.latitud),
+                    longitude: parseFloat(sala.longitud)
+                  }}
+                  title={sala.bandera}
+                  description={sala.descripcion}
+                  image={logo}
+                />
+              );
+            }
+          })}
+        </MapView>
       </Container>
     );
   };
 }
 
 const mapStateToProps = state => ({
-  region: state.maps.region
+  region: state.maps.region,
+  salas: state.salas.salas
 });
 
 const mapDispatchToProps = {
-  GetRegionMaps
+  GetRegionMaps,
+  ListadoSalas
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Maps);
