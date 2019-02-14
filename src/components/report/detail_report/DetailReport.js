5637210ed54f0def8c792850d99a5fa720ef9711
jmolinao <jorge.molina17@inacapmail.cl>
2019-02-14 15:14:15 -0300

    Fix Build And FechaActualizacion
    - Elimino los datos del build que impiden compitar.
    - prueba para mostrar la fecha como viene en el servicio

--- src/components/report/detail_report/DetailReport.js
+++ src/components/report/detail_report/DetailReport.js
@@ -162,10 +162,7 @@ class DetailReport extends React.Component {
               fontSize: 12
             }}
           >
-            Fecha dato B2B: 
-            {moment(fecha_actualizacion)
-              .add(1, "d")
-              .fromNow()}
+            Fecha información B2B: { fecha_actualizacion }
           </Text>
         </View>
         <View
