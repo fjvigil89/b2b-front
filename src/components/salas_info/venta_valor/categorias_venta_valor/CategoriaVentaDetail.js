4b9642e64f06c0dced4cdbb78a46152f9f977fb1
Daniel Toro <d.toro@outlook.com>
2020-05-12 11:20:20 -0400

    refactor(CategoriaVentaDetail): Refactor design venta valor

--- src/components/salas_info/venta_valor/categorias_venta_valor/CategoriaVentaDetail.js
+++ src/components/salas_info/venta_valor/categorias_venta_valor/CategoriaVentaDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Image, Dimensions, Text, ScrollView } from 'react-native';
-
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@assets/native-base-theme/variables//commonColor';
 class CategoriasVentaValor extends React.Component {
   static propTypes = {
     nombreSala: PropTypes.string,
@@ -50,12 +51,50 @@ class CategoriasVentaValor extends React.Component {
     return value;
   };
 
+  semaforo = (ventaMtdMtdLyPorc) => {
+    if (parseFloat(ventaMtdMtdLyPorc) >= 100) {
+      return (
+        <Ionicons
+          style={{
+            color: 'green',
+            fontSize: 22,
+            marginRight: 2,
+          }}
+          name="ios-arrow-round-up"
+        />
+      );
+    } else if (parseFloat(ventaMtdMtdLyPorc) < 100) {
+      return (
+        <Ionicons
+          style={{
+            color: 'red',
+            fontSize: 22,
+            marginRight: 2,
+          }}
+          name="ios-arrow-round-down"
+        />
+      );
+    } else {
+      return <></>;
+    }
+  };
+
   render() {
     const nombre = this.props.data[0];
-    const mtd = this.props.data[1].mtd ? this.formatter(this.props.data[1].mtd) : 0;
-    const mtdLy = this.props.data[1].mtdLy ? this.formatter(this.props.data[1].mtdLy) : 0;
-    const ytd = this.props.data[1].ytd ? this.formatter(this.props.data[1].ytd) : 0;
-    const ytdLy = this.props.data[1].ytdLy ? this.formatter(this.props.data[1].ytdLy) : 0;
+    const mtd = this.props.data[1].mtd
+      ? `$${this.formatter(this.props.data[1].mtd)}`
+      : `-`;
+    const mtdLy = this.props.data[1].mtdLy
+      ? `$${this.formatter(this.props.data[1].mtdLy)}`
+      : `-`;
+
+    const ventaMtdMtdLyPorc =
+      this.props.data[1].mtd > 0 && this.props.data[1].mtdLy > 0
+        ? `${(
+            (this.props.data[1].mtd * 100) /
+            this.props.data[1].mtdLy
+          ).toFixed(1)}%`
+        : `-`;
 
     return (
       <View
@@ -66,7 +105,7 @@ class CategoriasVentaValor extends React.Component {
       >
         <View
           style={{
-            flex: 1,
+            flex: 0.3,
             flexDirection: 'row',
           }}
         >
@@ -81,8 +120,6 @@ class CategoriasVentaValor extends React.Component {
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'flex-start',
-                borderBottomColor: '#DEDEDE',
-                borderBottomWidth: 1,
                 paddingLeft: 15,
               }}
             >
@@ -91,7 +128,7 @@ class CategoriasVentaValor extends React.Component {
                   fontSize: 12,
                   fontFamily: 'Bree',
                   fontWeight: 'bold',
-                  color: 'green'
+                  color: Colors.brandPrimary,
                 }}
               >
                 {nombre}
@@ -109,8 +146,31 @@ class CategoriasVentaValor extends React.Component {
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
-                borderBottomColor: '#DEDEDE',
-                borderBottomWidth: 1,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontFamily: 'Bree',
+                  fontWeight: 'bold',
+                  color: Colors.brandPrimary,
+                }}
+              >
+                VENTAS ($)
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flex: 0.35,
+              flexDirection: 'row',
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <Text
@@ -119,24 +179,35 @@ class CategoriasVentaValor extends React.Component {
                   fontFamily: 'Bree',
                   fontWeight: 'bold',
                 }}
-              >
-                MTD / YTD
-              </Text>
+              ></Text>
             </View>
           </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+          }}
+        >
           <View
             style={{
               flex: 0.35,
-              flexDirection: 'row',
+              flexDirection: 'column',
             }}
           >
             <View
               style={{
-                flex: 1,
+                flex: 0.3,
                 justifyContent: 'center',
-                alignItems: 'center',
+                alignItems: 'flex-start',
+                paddingLeft: 15,
+                borderRightColor: '#DEDEDE',
+                borderRightWidth: 1,
                 borderBottomColor: '#DEDEDE',
                 borderBottomWidth: 1,
+                borderTopColor: '#DEDEDE',
+                borderTopWidth: 1,
+                backgroundColor: '#F2F2F2',
               }}
             >
               <Text
@@ -144,172 +215,130 @@ class CategoriasVentaValor extends React.Component {
                   fontSize: 12,
                   fontFamily: 'Bree',
                   fontWeight: 'bold',
+                  color: '#6D6D6D',
                 }}
               >
-                MTD LY / YTD LY
+                VENTA (MTD)
               </Text>
             </View>
-          </View>
-        </View>
-        <View
-          style={{
-            flex: 0.75,
-            flexDirection: 'row',
-            padding: 0,
-          }}
-        >
-          <View
-            style={{
-              flex: 0.3,
-              justifyContent: 'center',
-              alignItems: 'flex-start',
-              paddingLeft: 15,
-              borderRightColor: '#DEDEDE',
-              borderRightWidth: 1,
-            }}
-          >
-            <Text
+            <View
               style={{
-                fontSize: 12,
-                fontFamily: 'Bree',
-                fontWeight: 'bold',
+                flex: 0.3,
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                borderBottomColor: '#DEDEDE',
+                borderRightColor: '#DEDEDE',
+                borderBottomWidth: 1,
+                borderRightWidth: 1,
+                paddingLeft: 15,
+                backgroundColor: '#DAE0F0',
               }}
             >
-              VENTAS (MES)
-            </Text>
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontFamily: 'Bree',
+                  fontWeight: 'bold',
+                  marginBottom: 0,
+                  color: '#6D6D6D',
+                }}
+              >
+                VENTA (MTD LY)
+              </Text>
+            </View>
           </View>
           <View
             style={{
               flex: 0.35,
-              flexDirection: 'row',
+              flexDirection: 'column',
             }}
           >
             <View
               style={{
-                flex: 1,
+                flex: 0.3,
                 justifyContent: 'center',
                 alignItems: 'center',
                 borderRightColor: '#DEDEDE',
                 borderRightWidth: 1,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 13,
-                  fontFamily: 'Questrial',
-                }}
-              >
-                ${mtd}
-              </Text>
-            </View>
-          </View>
-          <View
-            style={{
-              flex: 0.35,
-              flexDirection: 'row',
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 13,
-                  fontFamily: 'Questrial',
-                }}
-              >
-                ${mtdLy}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            flex: 0.75,
-            flexDirection: 'row',
-            padding: 0,
-          }}
-        >
-          <View
-            style={{
-              flex: 0.3,
-              justifyContent: 'center',
-              alignItems: 'flex-start',
-              borderBottomColor: '#DEDEDE',
-              borderRightColor: '#DEDEDE',
-              borderBottomWidth: 1,
-              borderRightWidth: 1,
-              paddingLeft: 15,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 12,
-                fontFamily: 'Bree',
-                fontWeight: 'bold',
-                marginBottom: 0,
-              }}
-            >
-              VENTAS (ANUAL)
-            </Text>
-          </View>
-          <View
-            style={{
-              flex: 0.35,
-              flexDirection: 'row',
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
                 borderBottomColor: '#DEDEDE',
                 borderBottomWidth: 1,
+                borderTopColor: '#DEDEDE',
+                borderTopWidth: 1,
+                backgroundColor: '#F2F2F2',
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontFamily: 'Questrial',
+                }}
+              >
+                {mtd}
+              </Text>
+            </View>
+            <View
+              style={{
+                flex: 0.3,
                 borderRightColor: '#DEDEDE',
                 borderRightWidth: 1,
+                borderBottomColor: '#DEDEDE',
+                borderBottomWidth: 1,
+                backgroundColor: '#DAE0F0',
               }}
             >
-              <Text
+              <View
                 style={{
-                  fontSize: 13,
-                  fontFamily: 'Questrial',
-                  color: 'black',
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}
               >
-                ${ytd}
-              </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    fontFamily: 'Questrial',
+                  }}
+                >
+                  {mtdLy}
+                </Text>
+              </View>
             </View>
           </View>
-
           <View
             style={{
               flex: 0.35,
-              flexDirection: 'row',
+              flexDirection: 'column',
             }}
           >
             <View
               style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
+                flex: 0.6,
+                borderTopColor: '#DEDEDE',
+                borderTopWidth: 1,
+                borderRightColor: '#DEDEDE',
+                borderRightWidth: 1,
                 borderBottomColor: '#DEDEDE',
                 borderBottomWidth: 1,
               }}
             >
-              <Text
+              <View
                 style={{
-                  fontSize: 13,
-                  fontFamily: 'Questrial',
-                  color: 'black',
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'row',
                 }}
               >
-                ${ytdLy}
-              </Text>
+                {this.semaforo(ventaMtdMtdLyPorc)}
+                <Text
+                  style={{
+                    fontSize: 22,
+                    fontFamily: 'Questrial',
+                  }}
+                >
+                  {ventaMtdMtdLyPorc}
+                </Text>
+              </View>
             </View>
           </View>
         </View>
