167e7c2df68b15cb2126d48ab42f1706862d0fca
Nicolás Carreño <n.carrenoh2019@gmail.com>
2020-07-15 15:16:33 -0400

    Feature: (hooks) hooks to manipulate the main AppState

---
+++ src/hooks/app-state-hook.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { AppState } from 'react-native';
+
+const useAppState = () => {
+  const [appState, setAppState] = useState(AppState.currentState);
+  useEffect(() => {
+    const handleAppEvent = (state) => {
+      if (state !== appState) {
+        setAppState(state);
+      }
+    };
+    AppState.addEventListener('change', handleAppEvent);
+    return () => {
+      AppState.removeEventListener('change', handleAppEvent);
+    };
+  }, [appState]);
+  return appState;
+};
+
+export const removeListener = (eventHandler) => {
+  AppState.removeEventListener('change', eventHandler);
+};
+
+export const usePrevAppState = (appState) => {
+  const appStateRef = useRef(appState);
+  useEffect(() => {
+    appStateRef.current = appState;
+  }, [appState]);
+  return appStateRef.current;
+};
+
+export default useAppState;
